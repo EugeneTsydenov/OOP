bed4761451d78fdfd065cdcce8ba9f8a99b8847e
EugeneTsydenov <ecydenov110@gmail.com>
2026-09-13 18:55:24 +0700

    chore

--- Task_1_1_1/src/main/java/ru/nsu/tsydenov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/tsydenov/Main.java
@@ -2,7 +2,7 @@ package ru.nsu.tsydenov;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {5, 2, 3, 1, 4, 212, 21, 9, 12, 2, 1212, 231};
+        int[] arr = {5, 2, 3, 1, 4};
         HeapSort.execute(arr);
     }
 }
