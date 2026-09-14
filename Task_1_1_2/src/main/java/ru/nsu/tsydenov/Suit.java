b470025dd6b818d7d936aec04439987fa2c65d77
EugeneTsydenov <ecydenov110@gmail.com>
2026-09-14 11:18:01 +0700

    feat: add Suit enum with fromIndex and toIndex converters

---
+++ Task_1_1_2/src/main/java/ru/nsu/tsydenov/Suit.java
@@ -0,0 +1,27 @@
+package ru.nsu.tsydenov;
+
+public enum Suit {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES;
+
+    public static Suit fromIndex(int n) {
+        return switch (n) {
+            case 0 -> CLUBS;
+            case 1 -> DIAMONDS;
+            case 2 -> HEARTS;
+            case 3 -> SPADES;
+            default -> throw new IllegalArgumentException("invalid index suit");
+        };
+    }
+
+    public int toIndex() {
+        return switch (this) {
+            case SPADES -> 0;
+            case CLUBS -> 1;
+            case HEARTS -> 2;
+            case DIAMONDS -> 3;
+        };
+    }
+}
