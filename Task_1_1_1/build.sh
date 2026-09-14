40dac1639aa7489f2329215e1beab7e62a700a68
EugeneTsydenov <ecydenov110@gmail.com>
2026-09-14 09:07:34 +0700

    chore: refactor build.sh and add jacoco to build.gradle

--- Task_1_1_1/build.sh
+++ Task_1_1_1/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-rm -rf out doc app.jar
-mkdir out doc
+rm -rf build
+mkdir -p build/classes build/docs build/libs
 
 # Pass the found files via xargs with a \0 delimiter.
-find src/main/java -name "*.java" -print0 | xargs -0 javac -d out
+find src/main/java -name "*.java" -print0 | xargs -0 javac -d build/classes
 
-javadoc -d doc -sourcepath src/main/java -subpackages ru -quiet
+javadoc -d build/docs -sourcepath src/main/java -subpackages ru -quiet
 
-jar --create --file out/app.jar --main-class ru.nsu.tsydenov.Main -C out .
+jar --create --file build/libs/app.jar --main-class ru.nsu.tsydenov.Main -C build/classes .
 
-java -jar out/app.jar
+java -jar build/libs/app.jar
