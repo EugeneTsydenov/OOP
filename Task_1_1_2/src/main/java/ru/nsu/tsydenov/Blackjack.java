c21fa26aedbaae5137f1ca33d7b794662ea6f202
EugeneTsydenov <ecydenov110@gmail.com>
2026-09-20 20:30:32 +0700

    feat: add blackjack class

---
+++ Task_1_1_2/src/main/java/ru/nsu/tsydenov/Blackjack.java
@@ -0,0 +1,17 @@
+package ru.nsu.tsydenov;
+
+public class Blackjack {
+    private Dealer dealer;
+    private Player player;
+    private Deck deck;
+
+    public Blackjack(Dealer dealer, Player player, Deck deck) {
+        this.dealer = dealer;
+        this.player = player;
+        this.deck = deck;
+    }
+
+    public void start() {
+
+    }
+}
