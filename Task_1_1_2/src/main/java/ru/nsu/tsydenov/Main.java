396a81a28ce8491eea506bd54187049414a99b10
EugeneTsydenov <ecydenov110@gmail.com>
2026-09-21 16:41:28 +0700

    feat: add start blackjack game entrypoint

--- Task_1_1_2/src/main/java/ru/nsu/tsydenov/Main.java
+++ Task_1_1_2/src/main/java/ru/nsu/tsydenov/Main.java
@@ -1,7 +1,11 @@
 package ru.nsu.tsydenov;
 
 public class Main {
-    static void main(String[] args) {
-
+    public static void main(String[] args) {
+        Deck deck = new Deck(1);
+        Player player = new Player();
+        Dealer dealer = new Dealer(deck);
+        Blackjack blackjack = new Blackjack(dealer, player);
+        blackjack.start();
     }
 }
