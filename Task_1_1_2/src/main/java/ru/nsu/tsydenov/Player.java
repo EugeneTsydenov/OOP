ed1d90a7c5130b5ea6b3e53180b119b97fc69917
EugeneTsydenov <ecydenov110@gmail.com>
2026-09-21 16:39:30 +0700

    feat: add start hand, calc total nominal with Ace, handString isBlackjack isBusted discardHand methods

--- Task_1_1_2/src/main/java/ru/nsu/tsydenov/Player.java
+++ Task_1_1_2/src/main/java/ru/nsu/tsydenov/Player.java
@@ -3,10 +3,18 @@ package ru.nsu.tsydenov;
 import java.util.ArrayList;
 
 public class Player {
+    protected final Card[] startHand = new Card[2];
     protected final ArrayList<Card> hand;
 
     public Player() {
         hand = new ArrayList<Card>();
+    }
+
+    public void hitStartHand(Card card1, Card card2) {
+        startHand[0] = card1;
+        startHand[1] = card2;
+        hand.add(card1);
+        hand.add(card2);
     }
 
     public void hit(Card card) {
@@ -15,10 +23,38 @@ public class Player {
 
     public int totalHandNominal() {
         int totalNominal = 0;
-        for (Card card: hand) {
-            totalNominal += card.getNominal();
+        int aceCount = 0;
+
+        for (Card card : hand) {
+            int val = card.getNominal();
+            totalNominal += val;
+            if (card.getName() == CardName.ACE) {
+                aceCount++;
+            }
+        }
+
+        while (totalNominal > 21 && aceCount > 0) {
+            totalNominal -= 10;
+            aceCount--;
         }
 
         return totalNominal;
+    }
+
+
+    public String handString() {
+        return hand.toString();
+    }
+
+    public boolean isBlackjack() {
+        return hand.size() == 2 && totalHandNominal() == 21;
+    }
+
+    public boolean isBusted() {
+        return totalHandNominal() > 21;
+    }
+
+    public void discardHand() {
+        hand.clear();
     }
 }
