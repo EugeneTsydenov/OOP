1fd213cfab1321f5d6b45550b142fccb915a962b
EugeneTsydenov <ecydenov110@gmail.com>
2026-09-21 16:40:27 +0700

    feat: add isOpenedCard logic, dealStartHand method

--- Task_1_1_2/src/main/java/ru/nsu/tsydenov/Dealer.java
+++ Task_1_1_2/src/main/java/ru/nsu/tsydenov/Dealer.java
@@ -1,11 +1,55 @@
 package ru.nsu.tsydenov;
 
 public class Dealer extends Player {
-    public Dealer() {
+    private Deck deck;
+    private boolean isOpenedCard;
+
+    public Dealer(Deck deck) {
         super();
+        this.deck = deck;
+        isOpenedCard = false;
     }
 
-    public void deal(Deck deck, Player player) {
+    public void startNewRound() {
+        deck.resetAndShuffle();
+    }
 
+    public void dealStartHand(Player player) {
+        player.hitStartHand(deck.takeCard(), deck.takeCard());
+        this.hitStartHand(deck.takeCard(), deck.takeCard());
+    }
+
+    public Card deal(Player receiverPlayer) {
+        Card card = deck.takeCard();
+        receiverPlayer.hit(card);
+        return card;
+    }
+
+    public Card openCloseCard() {
+        isOpenedCard = true;
+        return startHand[1];
+    }
+
+    public boolean isOpenedCard() {
+        return isOpenedCard;
+    }
+
+    @Override
+    public String handString() {
+        if (isOpenedCard) {
+            return hand.toString();
+        }
+
+        return "[" + startHand[0] + ", " + "<hole card>]";
+    }
+
+    public boolean shouldDealerDraw() {
+        return totalHandNominal() <  17;
+    }
+
+    @Override
+    public void discardHand() {
+        super.discardHand();
+        isOpenedCard = false;
     }
 }
